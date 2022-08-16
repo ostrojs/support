b4358260a959704f03a4f0bffffe72463f7252c6
developeramarkumar <30429306+developeramarkumar@users.noreply.github.com>
2022-08-16 21:26:39 +0530

    collection

--- collection.js
+++ collection.js
@@ -15,98 +15,136 @@ const Collect = (function() {
             return this[kUpdateData](lodash.flatMap(this.$items, cb))
 
         }
+
         forEach(cb) {
             return this[kUpdateData](lodash.forEach(this.$items, cb))
         }
+
         flatMap(iteratee, depth = 1) {
             return this[kUpdateData](lodash.flatMapDepth(this.$items, iteratee, depth))
         }
+
         flatten(depth = 1) {
             return this[kUpdateData](lodash.flattenDepth(this.$items, depth))
         }
+
         sortBy(iteratees) {
             return this[kUpdateData](lodash.sortBy(this.$items, iteratees))
         }
 
+
         reject(predicate) {
             return this[kUpdateData](lodash.reject(this.$items, predicate))
         }
+
         unique() {
             return this[kUpdateData](lodash.uniq(this.$items))
         }
 
+
         where(clause) {
             return this[kUpdateData](lodash.filter(this.$items, clause))
         }
-        take(clause) {
-            return lodash[((typeof clause == 'function') ? 'pickBy' : 'pick')](this.$items, clause)
+
+        take(clause,position = 'left') {
+            if(!['left','right'].includes(position)){
+                throw Error('Only left or right are allowed.')
+            }
+            position = position == 'left' ? 'take' : 'takeRight';
+            return this[kUpdateData](lodash[position](this.$items, clause))
         }
+
+        index(clause) {
+            return this[kUpdateData](lodash[((typeof clause == 'function') ? 'pickBy' : 'pick')](this.$items, clause))
+        }
+
         reduce(clause, init = 0) {
             return this[kUpdateData](lodash.reduce(this.$items, clause, init))
         }
+
         chunk(devide) {
             return this[kUpdateData](lodash.chunk(this.$items, devide))
         }
 
+
         concat(arr) {
             return this[kUpdateData](super.concat(arr))
         }
+
         contains(clause) {
             return this[kUpdateData](lodash.includes(this.$items, clause))
         }
+
         count() {
             return this[kUpdateData](lodash.size(this.$items))
         }
+
         countBy(clause) {
             return this[kUpdateData](lodash.countBy(this.$items, clause))
         }
+
         difference(array) {
             return this[kUpdateData](lodash.difference(this.$items, array))
         }
+
         differenceBy(array, clause) {
             return this[kUpdateData](lodash.differenceBy(this.$items, array, clause))
         }
+
         differenceWith(array, clause) {
             return this[kUpdateData](lodash.differenceWith(this.$items, array, clause))
         }
+
 
         filter(clause) {
             return this[kUpdateData](lodash.filter(this.$items, clause))
         }
 
+
         groupBy(clause) {
             return this[kUpdateData](lodash.groupBy(this.$items, clause))
         }
+
         has(clause) {
             return this[kUpdateData](lodash.has(this.$items, clause))
         }
+
         keys() {
             return this[kUpdateData](lodash.keys(this.$items))
         }
+
         max(clause) {
             return lodash[(clause) ? 'maxBy' : 'max'](this.$items, clause)
         }
+
         min() {
             return lodash[(clause) ? 'minBy' : 'min'](this.$items, clause)
         }
+
         pull() {
             return this[kUpdateData](lodash.pull(this.$items, ...arguments))
         }
+
         join(clause) {
             return this[kUpdateData](lodash.join(this.$items, clause))
         }
+
         isEmpty() {
             return this[kUpdateData](lodash.isEmpty(this.$items))
         }
+
         intersection(array) {
             return this[kUpdateData](lodash.intersection(this.$items, array))
         }
+
         intersectionBy(array, clause) {
             return this[kUpdateData](lodash.intersectionBy(this.$items, array, clause))
         }
+
         keyBy(clause) {
             return this[kUpdateData](lodash.keyBy(this.$items, clause))
         }
+
         pluck(first, second) {
             let results = second ? {} : []
             for (let data of this.$items) {
@@ -118,32 +156,41 @@ const Collect = (function() {
             }
             return results
         }
+
         push() {
             return this[kUpdateData](lodash.concat(this.$items, ...arguments))
         }
+
         put(key, value) {
             return this[kUpdateData](lodash.assign(this.$items, {
                 [key]: value
             }))
         }
+
         shuffle() {
             return this[kUpdateData](lodash.shuffle(this.$items))
         }
+
         without() {
             return this[kUpdateData](lodash.without(this.$items, ...arguments))
         }
+
         skip(end) {
             return this[kUpdateData](lodash.slice(this.$items, 0, end))
         }
+
         first() {
             return this[kUpdateData](lodash.head(this.$items))
         }
+
         last() {
             return this[kUpdateData](lodash.last(this.$items))
         }
+
         firstWhere(clause) {
             return this.where(clause).first()
         }
+
         get(clause) {
             return this[kUpdateData](lodash.get(this.$items, clause))
         }
@@ -156,8 +203,26 @@ const Collect = (function() {
             return this.$items
         }
 
+        toArray() {
+            return this.all();
+        }
+
         toJSON() {
-            return this.all()
+            return { ...this.all() };
+        }
+
+        toJson() {
+            return this.toJSON();
+        }
+
+        serialize() {
+            let $value = this.all();
+            if (Array.isArray($value)) {
+                return this.toArray();
+
+            } else {
+                return this.toJSON();
+            }
         }
 
         [Symbol.iterator]() {
@@ -187,10 +252,10 @@ const Collect = (function() {
     }
 })()
 
-class Collection extends Macroable.extend(CollectionInterface) {
+class Collection extends Macroable.extend(Collect) {
 
     constructor(data) {
-        super()
+        super(data)
         this.$items = data
         this.length = data.length
     }
