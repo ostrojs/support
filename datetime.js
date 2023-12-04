e2ea9cb290fa1a2d86d5055180385233f7227733
Amar kumar singh <amar@ostrojs.com>
2023-12-04 12:01:43 +0530

    new feature added with DEFAULT_CLASS_NAME

--- datetime.js
+++ datetime.js
@@ -3,29 +3,33 @@ const InvalidArgumentException = require('./exceptions/invalidArgumentException'
 const { Macroable } = require('./macro')
 class DateTimeZone extends Macroable {
 
+    static DEFAULT_CLASS_NAME = DateTime;
+
     static $callable = null;
 
     static $factory = null;
+
+    static $dateClass = null;
 
     static getTimeZone() {
         return process.env.TZ
     }
 
     static get DATE_TIME() {
-        return DateTime.utc().setZone(this.getTimeZone())
+        return this.DEFAULT_CLASS_NAME.utc().setZone(this.getTimeZone())
     }
 
     static create($year = 0, $month = 0, $day = 0, $hour = 0, $minute = 0, $second = 0, $tz = null) {
-        return DateTime.utc($year, $month, $day, $hour, $minute, $second).setZone($tz || this.getTimeZone())
+        return this.DEFAULT_CLASS_NAME.utc($year, $month, $day, $hour, $minute, $second).setZone($tz || this.getTimeZone())
     }
     static createFromDate($year = null, $month = null, $day = null, $tz = null) {
-        return DateTime.utc($year, $month, $day, 0, 0, 0).setZone($tz || this.getTimeZone())
+        return this.DEFAULT_CLASS_NAME.utc($year, $month, $day, 0, 0, 0).setZone($tz || this.getTimeZone())
     }
     static createFromFormat($format, $time, $tz = null) {
-        return DateTime.fromFormat($time, $format, { setZone: $tz || this.getTimeZone() })
+        return this.DEFAULT_CLASS_NAME.fromFormat($time, $format, { setZone: $tz || this.getTimeZone() })
     }
     static createFromTime($hour = 0, $minute = 0, $second = 0, $tz = null) {
-        return DateTime.fromFormat($time, $format, { setZone: $tz || this.getTimeZone() })
+        return this.DEFAULT_CLASS_NAME.fromFormat($time, $format, { setZone: $tz || this.getTimeZone() })
     }
 
     static now($tz = null) {
@@ -51,49 +55,57 @@ class DateTimeZone extends Macroable {
     static useDefault() {
         this.$callable = null;
         this.$factory = null;
+        this.$dateClass = null;
     }
 
     static useCallable($callable) {
         this.$callable = $callable;
-
         this.$factory = null;
+        this.$dateClass = null;
     }
 
     static useClass($dateClass) {
         this.$dateClass = $dateClass;
-
         this.$factory = null;
         this.$callable = null;
     }
 
     static useFactory($factory) {
         this.$factory = $factory;
-
         this.$callable = null;
+        this.$dateClass = null;
     }
 
     __call($target, $method, $parameters) {
-        $target = $target.constructor
+        const $defaultClassName = this.constructor.DEFAULT_CLASS_NAME;
+
+        $target = $target.constructor;
+
         if ($target.$callable) {
-            return $target.$callable($target[$method](...$parameters));
+            return $target.$callable($defaultClassName[$method](...$parameters));
         }
 
         if ($target.$factory) {
             return $target.$factory[$method](...$parameters);
         }
 
-        if (method_exists($target, $method) ||
-            method_exists($target, 'hasMacro') && $target.hasMacro($method)) {
+        if (method_exists($target, $method)) {
             return $target[$method](...$parameters);
         }
 
-        let $date = $target[$method](...$parameters);
+        const $dateClass = this.constructor.$dateClass || $defaultClassName;
 
-        if (method_exists($target, 'instance')) {
-            return $target.instance($date);
+        if (method_exists($dateClass, $method)) {
+            return $dateClass[$method](...$parameters);
         }
 
-        return $date
+        const $date = $defaultClassName[$method](...$parameters);
+
+        if (method_exists($dateClass, 'instance')) {
+            return $dateClass.instance($date);
+        }
+
+        return new $dateClass($date.format('Y-m-d H:i:s.u'), $date.getTimezone());
     }
 
 }
